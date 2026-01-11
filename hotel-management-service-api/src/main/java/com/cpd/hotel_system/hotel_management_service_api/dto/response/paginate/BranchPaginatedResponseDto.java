fa3bb3d18f70b6ae83323ddaf2388c95b4d03f82
oshanmanidu <oshanmanidu12@gmail.com>
2026-01-11 23:03:55 +0530

    paginated

---
+++ hotel-management-service-api/src/main/java/com/cpd/hotel_system/hotel_management_service_api/dto/response/paginate/BranchPaginatedResponseDto.java
@@ -0,0 +1,17 @@
+package com.cpd.hotel_system.hotel_management_service_api.dto.response.paginate;
+
+import com.cpd.hotel_system.hotel_management_service_api.dto.response.ResponseBranchDto;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class BranchPaginatedResponseDto {
+    private List<ResponseBranchDto> dataList;
+    private Long dataCount;
+}
